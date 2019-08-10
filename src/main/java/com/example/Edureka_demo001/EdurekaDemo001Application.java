78307c6dddd5580d0cb9bde7f08628e933aa3c27
ppknet <praveen.kanase@gmail.com>
2019-08-10 10:18:46 +0800

    project initialization

---
+++ src/main/java/com/example/Edureka_demo001/EdurekaDemo001Application.java
@@ -0,0 +1,13 @@
+package com.example.Edureka_demo001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdurekaDemo001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdurekaDemo001Application.class, args);
+	}
+
+}
